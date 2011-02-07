d4d55a06b24cf045f79260a5c4ef5d3001c8ba7d
Jérôme Tamarelle <jerome@tamarelle.net>
2011-02-08 00:28:14 +0100

    Use Git repo for vendors

--- app/autoload.php
+++ app/autoload.php
@@ -2,23 +2,25 @@
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
+$vendor = __DIR__.'/../vendor_full';
+
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'                        => __DIR__.'/../vendor/symfony/src',
+    'Symfony'                        => $vendor.'/symfony/src',
     'Sensio'                         => __DIR__.'/../src',
     'Git'                            => __DIR__.'/../src',
-    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-data-fixtures/lib',
-    'Doctrine\\Common'               => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL\\Migrations'     => __DIR__.'/../vendor/doctrine-migrations/lib',
-    'Doctrine\\MongoDB'              => __DIR__.'/../vendor/doctrine-mongodb/lib',
-    'Doctrine\\ODM\\MongoDB'         => __DIR__.'/../vendor/doctrine-mongodb-odm/lib',
-    'Doctrine\\DBAL'                 => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'                       => __DIR__.'/../vendor/doctrine/lib',
-    'Zend'                           => __DIR__.'/../vendor/zend/library',
+    'Doctrine\\Common\\DataFixtures' => $vendor.'/doctrine-data-fixtures/lib',
+    'Doctrine\\Common'               => $vendor.'/doctrine-common/lib',
+    'Doctrine\\DBAL\\Migrations'     => $vendor.'/doctrine-migrations/lib',
+    'Doctrine\\MongoDB'              => $vendor.'/doctrine-mongodb/lib',
+    'Doctrine\\ODM\\MongoDB'         => $vendor.'/doctrine-mongodb-odm/lib',
+    'Doctrine\\DBAL'                 => $vendor.'/doctrine-dbal/lib',
+    'Doctrine'                       => $vendor.'/doctrine/lib',
+    'Zend'                           => $vendor.'/zend/library',
 ));
 $loader->registerPrefixes(array(
-    'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
-    'Twig_'            => __DIR__.'/../vendor/twig/lib',
-    'Swift_'           => __DIR__.'/../vendor/swiftmailer/lib/classes',
+    'Twig_Extensions_' => $vendor.'/twig-extensions/lib',
+    'Twig_'            => $vendor.'/twig/lib',
+    'Swift_'           => $vendor.'/swiftmailer/lib/classes',
 ));
 $loader->register();
