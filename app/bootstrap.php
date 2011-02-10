baa3f483e318ca2a97dd15ffa48ba9e13e4186d6
Jérôme Tamarelle <jerome@tamarelle.net>
2011-02-10 23:40:15 +0100

    Configured application

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
-require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/HttpKernel/bootstrap.php';
+require_once __DIR__.'/../vendor_full/symfony/src/Symfony/Component/HttpKernel/bootstrap.php';
 require_once __DIR__.'/autoload.php';
